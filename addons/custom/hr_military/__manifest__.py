7f9cdd083413ac9ebf59e61799a6b32a57f56b0a
Vladyslav <vladyslav.lekh@softcostruct.com>
2025-12-17 15:22:58 +0200

    "Update: tck_sp_views menuitem / Add: Image hr_military "

--- addons/custom/hr_military/__manifest__.py
+++ addons/custom/hr_military/__manifest__.py
@@ -1,14 +1,16 @@
 {
-    "name": "HR Military",
-    "version": "17.0.1.0.0",
-    "depends": ["hr"],
-    "data": [
-        "security/ir.model.access.csv",
-        "views/tck_sp_views.xml",
-        "views/hr_employee_views.xml",
-    ],
-    "installable": True,
-    "application": True,
-    "license": "LGPL-3",
-
+  "name": "HR Military",
+  "version": "17.0.1.0.0",
+  "depends": ["hr"],
+  "author": "Author / Vlad",
+  "category": "Human Resources",
+  "license": "LGPL-3",
+  "application": True,
+  "installable": True,
+  "summary": "Військовий облік для співробітників",
+  "data": [
+    "security/ir.model.access.csv",
+    "views/tck_sp_views.xml",
+    "views/hr_employee_views.xml",
+  ],
 }
